06ee5ca01e8bbeade143755d2457e05468174b42
NahueTomas <nahuetomas@outlook.com>
2021-07-22 18:43:14 -0300

    Avanzando en la estructura del proyecto

--- routes/app.js
+++ routes/app.js
@@ -2,12 +2,26 @@ const { Router } = require('express')
 
 const router = Router()
 
+// TODO: Leer datos de la base de datos
 const fs = require('fs')
 const rawdata = fs.readFileSync('datos.json')
 const data = JSON.parse(rawdata)
 
+// TODO: Cambiar URL en producción
+
 router.get('/', (req, res) => {
-    res.render( 'app.pug',  {pagina : "STUDY", datos: data})
+    res.render( 'app.pug',  {pagina : "STUDY", datos: data, crear: "nueva-materia"})
+})
+
+router.get('/materia/:id', (req, res) => {
+    let datos = {}
+    data.materias.forEach( materia => {
+        if (materia.id == req.params.id){
+            datos = materia
+        }
+    })
+
+    res.render( 'materia.pug',  {pagina : "STUDY - materia", datos, id: req.params.id})
 })
 
 module.exports = router
