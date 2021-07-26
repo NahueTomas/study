8b38e30207aa734485661fc95d5ab6f74ebca1e5
NahueTomas <nahuetomas@outlook.com>
2021-07-25 22:54:08 -0300

    Ultimos detalles para deploy

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const { dbConnection } = require('./bdd')
 class Server {
     constructor(){
         this.app = express()
-        this.port = process.env.PUERTO
+        this.port = process.env.PUERTO || process.env.PORT
 
         this.rutas = {
             login: '/auth',
