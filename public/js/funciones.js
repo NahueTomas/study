0907880e35c24fa5d6f6e54082fb99b3103740ac
NahueTomas <nahuetomas@outlook.com>
2021-08-15 22:31:41 -0300

    BETA 1.2

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -4,48 +4,81 @@ export const traductor = (contenedor, contenido) => {
 
     arrayContenido.forEach( parrafo => {
         
-        let $linea
+        let $linea = ""
 
+        // ESPACIO
         if( parrafo.trim() == ""){
             $linea = document.createElement('br')
-        } else if ( parrafo.trim() == "="){
+        }
+        // LÍNEA
+        else if ( parrafo.trim() == "="){
             $linea = document.createElement('hr')
-        } else if (parrafo.trim().split('')[0] == "-"){
+        }
+        // LISTA CHICA
+        else if (parrafo.trim().split('')[0] == "-"){
             $linea = document.createElement('li')
             $linea.classList.add('lista-chica')
-            $linea.textContent = parrafo.slice(1)
-        } else if (parrafo.trim().split('')[0] == "+"){
+
+            if (parrafo.trim().includes("&")){
+                const arrayTexto = parrafo.slice(1).split('&')
+                const contador = arrayTexto.length - 1
+    
+                for (let i = 0; i <= contador; i++ ){
+                    if( i % 2 != 0 ){
+                        $linea.innerHTML += `<span class="tapado">${arrayTexto[i]}</span>`
+                    }else{
+                        $linea.innerHTML += `${arrayTexto[i]}`
+                    }
+                }
+            } else{$linea.textContent = parrafo.slice(1)}
+        } 
+        // LISTA GRANDE
+        else if (parrafo.trim().split('')[0] == "+"){
             $linea = document.createElement('li')
             $linea.classList.add('lista-grande')
-            $linea.textContent = parrafo.slice(1)
-        } else if(parrafo.trim().split('')[0] == "#"){
+
+            if (parrafo.trim().includes("&")){
+                const arrayTexto = parrafo.slice(1).split('&')
+                const contador = arrayTexto.length - 1
+    
+                for (let i = 0; i <= contador; i++ ){
+                    if( i % 2 != 0 ){
+                        $linea.innerHTML += `<span class="tapado">${arrayTexto[i]}</span>`
+                    }else{
+                        $linea.innerHTML += `${arrayTexto[i]}`
+                    }
+                }
+            } else{$linea.textContent = parrafo.slice(1)}
+        }
+        // TÍTULO
+        else if(parrafo.trim().split('')[0] == "#"){
             $linea = document.createElement('h3')
             $linea.textContent = parrafo.slice(1)
-        } else if(parrafo.trim().split('')[0] == "{"){
+        }
+        // FÓRMULA
+        else if(parrafo.trim().split('')[0] == "{"){
             $linea = document.createElement('div')
             $linea.classList.add('formula')
             $linea.textContent = parrafo.slice(1)
-        } else{
+        } 
+        // PÁRRAFO
+        else{
             $linea = document.createElement('p')
             $linea.classList.add('contenido-carta-vista')
-            $linea.textContent = parrafo
-        }
-        
-        if(parrafo.trim().includes("&")){            
-            $linea = document.createElement('p')
-            const arrayTexto = parrafo.split('&')
-            const contador = arrayTexto.length - 1
-            $linea.innerHTML = ""
+            
+            if(parrafo.trim().includes("&")){            
+                const arrayTexto = parrafo.split('&')
+                const contador = arrayTexto.length - 1
 
-            for (let i = 0; i < contador; i++ ){
-                if( i % 2 != 0 ){
-                    $linea.innerHTML += `<span class="tapado">${arrayTexto[i]}</span>`
-                }else{
-                    $linea.innerHTML += `${arrayTexto[i]}`
+                for (let i = 0; i <= contador; i++ ){
+                    if( i % 2 != 0 ){
+                        $linea.innerHTML += `<span class="tapado">${arrayTexto[i]}</span>`
+                    }else{
+                        $linea.innerHTML += `${arrayTexto[i]}`
+                    }
                 }
-            }
+            } else{$linea.textContent = parrafo}
         }
-        
         contenedor.appendChild($linea)
     })
 }
